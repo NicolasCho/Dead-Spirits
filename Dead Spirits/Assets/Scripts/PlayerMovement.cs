3249ab62c5b9dc04dd64b8b533ba7d681fff91d8
NicolasCho <nicolasbkc@gmail.com>
2023-03-20 11:38:29 -0300

    player movement

---
+++ Dead Spirits/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Camera cam;
+    public float runSpeed = 20.0f;
+
+    private Vector2 mousePos;
+
+    float horizontal;
+    float vertical;
+
+    
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>(); 
+        cam = Camera.main;
+    }
+
+    void Update(){
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    private void FixedUpdate(){  
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        // int animationIdx = ChangeSprite(angle);
+    }
+}
