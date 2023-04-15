14e97e141c131e777f0319846285920e5e662343
NicolasCho <nicolasbkc@gmail.com>
2023-04-14 23:11:06 -0300

    hitboxes e inimigo

--- Dead Spirits/Assets/Scripts/CombatScripts/EnemyManager.cs
+++ Dead Spirits/Assets/Scripts/CombatScripts/EnemyManager.cs
@@ -11,9 +11,4 @@ public class EnemyManager : MonoBehaviour
         HP -= 1;
     }
 
-    public void onCollisionEnter2D(Collision2D other){
-        if (other.gameObject.tag == "PlayerAttack"){
-            TakeDamage();
-        }
-    }
 }
