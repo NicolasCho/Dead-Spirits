481d2c76055227ba19888946d86c1f77c1cf0d58
NicolasCho <nicolasbkc@gmail.com>
2023-04-24 15:47:10 -0300

    skeleton prefab

--- Dead Spirits/Assets/Scripts/EnemyMov/SkeletonMovement.cs
+++ Dead Spirits/Assets/Scripts/EnemyMov/SkeletonMovement.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 using System.Collections;
 public class SkeletonMovement : MonoBehaviour{
     public Transform target;
-    public float speed = 2f;
-    public float range = 8f;
+    public float speed = 1f;
+    public float range = 15f;
     private float distance;
 
-    public float attackRange;
     public Animator animator;
     public bool canMove = true;
     public bool damagedTime = false;
@@ -17,10 +16,7 @@ public class SkeletonMovement : MonoBehaviour{
         distance = Vector2.Distance(transform.position, target.position);
 
         if(canMove){
-           if (distance <= attackRange){
-                //StartCoroutine(AttackCoroutine());
-            }
-            else if (distance <= range && !damagedTime)
+            if (distance <= range && !damagedTime)
             {
                 transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
                 animator.SetFloat("deltax",transform.position.x - target.position.x);
@@ -31,55 +27,43 @@ public class SkeletonMovement : MonoBehaviour{
             } 
         }
         
+    }   
+
+    IEnumerator stopMovement(bool kill){
+        damagedTime = true;
+        float time;
+        float timer = 0f;
+        
+        time = 2f;
+        while(timer < time){
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        if (kill){
+            Instantiate(spirit, transform.position, Quaternion.identity);
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.enabled = false;
+            yield return new WaitForSeconds(2f);
+            Destroy(this.gameObject);
+        }    
+        damagedTime = false;
     }
 
-    // IEnumerator AttackCoroutine()
-    // {   
-    //     yield return new WaitForSeconds(1f);
-    //     if (!damagedTime)
-    //       transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime*1f);
-    //     animator.SetTrigger("Attack");
-    //     yield return new WaitForSeconds(1f);
-    //     canMove = false;
-    //     yield return new WaitForSeconds(3f);
-    //     canMove = true;
-    // }
+    void OnTriggerEnter2D(Collider2D other){
+        GameObject attacker = other.gameObject;
+        if (attacker.tag == "PlayerAttack" || attacker.tag == "SummonedSpirit"){
+            if (attacker.tag == "PlayerAttack")
+                attacker.GetComponentInParent<PlayerManager>().ComboSystem(false);
 
-    // IEnumerator stopMovement(bool kill){
-    //     damagedTime = true;
-    //     float time;
-    //     float timer = 0f;
-        
-    //     time = 2f;
-    //     while(timer < time){
-    //         timer += Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     if (kill){
-    //         Instantiate(spirit, transform.position, Quaternion.identity);
-    //         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-    //         sprite.enabled = false;
-    //         yield return new WaitForSeconds(2f);
-    //         Destroy(this.gameObject);
-    //     }    
-    //     damagedTime = false;
-    // }
-
-    // void OnTriggerEnter2D(Collider2D other){
-    //     GameObject attacker = other.gameObject;
-    //     if (attacker.tag == "PlayerAttack" || attacker.tag == "SummonedSpirit"){
-    //         if (attacker.tag == "PlayerAttack")
-    //             attacker.GetComponentInParent<PlayerManager>().ComboSystem(false);
-
-    //         GetComponent<EnemyManager>().TakeDamage();
-    //         if (GetComponent<EnemyManager>().HP == 0){
-    //             canMove=false;
-    //             animator.SetTrigger("Dead");
-    //             StartCoroutine(stopMovement(true));
-    //         }else{
-    //             animator.SetTrigger("Damaged");
-    //             StartCoroutine(stopMovement(false));
-    //         } 
-    //     }
-    // }
+            GetComponent<EnemyManager>().TakeDamage();
+            if (GetComponent<EnemyManager>().HP == 0){
+                canMove=false;
+                //animator.SetTrigger("Dead");
+                StartCoroutine(stopMovement(true));
+            }else{
+                //animator.SetTrigger("Damaged");
+                StartCoroutine(stopMovement(false));
+            } 
+        }
+    }
 }
