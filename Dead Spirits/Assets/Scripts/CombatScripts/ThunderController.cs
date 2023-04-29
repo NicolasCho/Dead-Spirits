2fe16fb12f5428c228da4da0d6e666d61a87c0b6
NicolasCho <nicolasbkc@gmail.com>
2023-04-29 16:14:28 -0300

    fazendo o boss

---
+++ Dead Spirits/Assets/Scripts/CombatScripts/ThunderController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderController : MonoBehaviour
+{
+    float ActivationCD = 3.0f;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActivationCD -= Time.deltaTime;
+        if ( ActivationCD < 0 ){
+            StartCoroutine(ActivateMagic());
+            ActivationCD = 200f;
+        }
+    }
+
+    IEnumerator ActivateMagic()
+    {      
+        animator.SetTrigger("ActivateMagic");
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
